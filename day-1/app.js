73fea7ea6810aae8b79e6257309d37c21d2667a8
KO LI <soummyaskoli567@gmail.com>
2026-06-10 14:10:27 +0530

    day 1 added

---
+++ day-1/app.js
@@ -0,0 +1,41 @@
+// Variables in JavaScript  
+
+let age = 20;
+console.log(age);
+
+age = 21 
+
+console.log("My current age is :"+age);  
+console.log("My current age is :",age);  
+
+// String concatenation
+let first = "soumya";
+let last = "koli";
+
+let fullname = first + " " + last; 
+console.log("My full name is :"+fullname);  
+
+name = "Emma";
+console.log("name"); 
+
+let x = null
+console.log(x);  
+
+let y = undefined
+console.log(y); 
+
+// Primitive 
+let num = 20; 
+let str = "Hello, World!";  
+let bool = true;  
+let undef;  
+let nul = null; 
+
+// Non-primitive 
+let obj = { 
+    name: "name", 
+    age: 20 
+};  
+
+let arr = [1, 2, 3, 4, 5];  // array    
+
